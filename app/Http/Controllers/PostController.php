99e3604c0bf5f3d1168f87e18fd29bfc7c539f06
kbysrnsk <krnsk1101@icloud.com>
2022-12-01 14:40:47 +0900

    this is commit 8-02

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,15 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Post;
 use Illuminate\Http\Request;
-
+use App\Models\Post;
 class PostController extends Controller
 {
-    public function index(Post $post)
+        public function index(Post $post)
     {
-        return view('posts/index')->with(['posts' => $post->get()]);  
-       //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
+        return view('posts/index')->with(['posts' => $post->getPaginateByLimit(1)]); 
     }
 }
-?>
